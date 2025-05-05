04425d450d9c0a9bc579e8d9ae3b20e1f9ca12bc
ValterTertuliano <valtertertuliano1994@gmail.com>
2025-05-05 10:56:35 -0300

    Update contrato_introducao.py

--- Estudando Python/Desafios Aleatorios/classes/contrato_introducao.py
+++ Estudando Python/Desafios Aleatorios/classes/contrato_introducao.py
@@ -5,10 +5,40 @@ class MeioDeTransporte(ABC):  # Classe abstrata = contrato
     def mover(self):
         pass  # qualquer classe concreta deve implementar isso
 
+    @abstractmethod
+    def combustivel(self):
+        pass
+
 class Carro(MeioDeTransporte):
+    @property
     def mover(self):
-        print("Dirigindo...")
+        return "Dirigindo..."
+
+    @property
+    def combustivel(self):
+        return "Combustão Gasolina"
 
 class Bicicleta(MeioDeTransporte):
+    @property
     def mover(self):
-        print("Pedalando...")
+        return "Pedalando..."
+
+    @property
+    def combustivel(self):
+        return "Energia Humana"
+
+
+lista = []
+for x in range(20):
+    carro = Carro()
+    bike = Bicicleta()
+    lista.append(carro)
+    lista.append(bike)
+
+for x in lista:
+    print("-"*65)
+    print("--- Dados de Veiculo")
+    print("-"*65)
+    print(f"Classe Atual: {x.__class__.__name__}")
+    print(f"Tipo de movimento: {x.mover}")
+    print(f"Tipo de combustivel: {x.combustivel}\n")
