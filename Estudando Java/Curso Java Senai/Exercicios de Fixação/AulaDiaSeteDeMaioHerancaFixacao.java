892405275e2daea7a1184ccfb990a6109169c078
ValterTertuliano <valtertertuliano1994@gmail.com>
2025-05-11 22:54:52 -0300

    mini-projeto - padrao mvc - gerenciador de faturas

--- Estudando Java/Curso Java Senai/Exercicios de Fixação/AulaDiaSeteDeMaioHerancaFixacao.java
+++ Estudando Java/Curso Java Senai/Exercicios de Fixação/AulaDiaSeteDeMaioHerancaFixacao.java
@@ -37,12 +37,77 @@ class Animal {
 class Leao extends Animal{
     private String territorio;
 
-    public Leao()
+    public Leao(String nome, int idade, String territorio){
+        super(nome, idade);
+        this.territorio = territorio;
+
+
+    }
+
+    public String getTerritorio(){
+        return this.territorio;
+    }
+
+    public void setTerritorio(String novoTerritorio){
+        this.territorio = novoTerritorio;
+    }
+
+    public void emitirSom(){ // usando polimorfismo
+        System.out.println("Rugindo ( leão )");
+    }
+
+    public void exibirDados(){
+        super.exibirDados();
+        System.out.println("Meu territorio é " + this.territorio);
+    }
+
+}
+
+class Papagaio extends Animal{
+    private int vocabulario;
+
+    public Papagaio(String nome, int idade, int vocabulario){
+        super(nome, idade);
+        this.vocabulario = vocabulario;
+    }
+
+    public int getVocabulario(){
+        return this.vocabulario;
+    }
+
+    public void setVocabulario(int novoVocabulario){
+        if (novoVocabulario > 0) {
+            this.vocabulario = novoVocabulario;
+        }
+    }
+
+    public void falar(String msg){
+        System.out.println("Papagaio Falando: " + msg);
+    }
+
+    public void emitirSom(){
+        System.out.println("Estou falando, sou um papagaio");
+    }
+
+    public void exibirDados(){
+        super.exibirDados();
+
+    }
 }
 
 public class AulaDiaSeteDeMaioHerancaFixacao {
 
     public static void main(String[] args) {
-        
+        Leao leao = new Leao("Valter", 30, "Brasileiro");
+        leao.emitirSom();
+        leao.exibirDados();
+        System.out.println();
+
+        Papagaio papagaio = new Papagaio("Valter", 28, 5);
+        papagaio.emitirSom();
+        papagaio.falar("Eu sou um Papagaio");
+        papagaio.exibirDados();
+
+
     }
 }
--- Estudando Java/Curso Java Senai/Exercicios de Fixação/]AulaDiaSeisDeMaioCadastroAlunos.java
+++ Estudando Java/Curso Java Senai/Exercicios de Fixação/]AulaDiaSeisDeMaioCadastroAlunos.java
@@ -1,4 +1,4 @@
-public class SistemaCadastroAlunos {
+public class AulaDiaSeisDeMaioCadastroAlunos {
 
     
     public static void main(String[] args) {
